30f420948f77ad6462d550bc490bfa1ac0237e5a
bdevapatla <reachbp4@gmail.com>
2018-07-14 17:00:39 -0400

    committing initial changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      speed:10
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const speedRef =  rootRef.child('speed');
+    speedRef.on('value',snap => {
+      this.setState({
+        speed:snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Reader's League</h1>
         </header>
         <p className="App-intro">
-          Analytics for books
+         {this.state.speed}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
+
+var config = {
+    apiKey: "AIzaSyAf5ja8pZl3_LY6UqoNM3OF0UHdT6RJ0zM",
+    authDomain: "readersleague-21e03.firebaseapp.com",
+    databaseURL: "https://readersleague-21e03.firebaseio.com",
+    projectId: "readersleague-21e03",
+    storageBucket: "readersleague-21e03.appspot.com",
+    messagingSenderId: "519989270169"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
